35edd1ea40188b61a15f1fb6540db2015b16953e
joyceoliveiragg <joyce.ggomes@ufpe.com.br>
2025-03-31 19:08:31 -0300

    Initial commit

---
+++ src/main/java/me/dio/Timer/App/TimerAppApplication.java
@@ -0,0 +1,13 @@
+package me.dio.Timer.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimerAppApplication.class, args);
+	}
+
+}
