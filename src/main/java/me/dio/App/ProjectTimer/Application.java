16b1f9b930440845a8d416c9af0e1586a2b5a9ea
joyceoliveiragg <joyce.ggomes@ufpe.com.br>
2025-04-03 00:14:59 -0300

    ajuste de erro

--- src/main/java/me/dio/App/ProjectTimer/Application.java
+++ src/main/java/me/dio/App/ProjectTimer/Application.java
@@ -3,8 +3,11 @@ package me.dio.App.ProjectTimer;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.logging.Logger;
 
 @OpenAPIDefinition(
 		servers = {
@@ -13,7 +16,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 )
 @SpringBootApplication
 public class Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
