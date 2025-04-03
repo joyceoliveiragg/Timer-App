b4b71eacf3f9f9b223a432ec0ef72406b2e84334
joyceoliveiragg <joyce.ggomes@ufpe.com.br>
2025-04-02 23:44:11 -0300

    ajuste de erro no pom.xml

--- src/test/java/me/dio/App/ProjectTimer/ApplicationTests.java
+++ src/test/java/me/dio/App/ProjectTimer/ApplicationTests.java
@@ -1,9 +1,9 @@
-package me.dio.App;
+package me.dio.App.ProjectTimer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = Application.class)
 class ApplicationTests {
 
 	@Test
