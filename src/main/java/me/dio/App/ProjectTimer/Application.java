dba82c4826acfff547d1637ab87a9c38dea0cd5f
joyceoliveiragg <joyce.ggomes@ufpe.com.br>
2025-04-03 00:28:46 -0300

    teste

--- src/main/java/me/dio/App/ProjectTimer/Application.java
+++ src/main/java/me/dio/App/ProjectTimer/Application.java
@@ -17,7 +17,11 @@ import java.util.logging.Logger;
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		try {
+			SpringApplication.run(Application.class, args);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
